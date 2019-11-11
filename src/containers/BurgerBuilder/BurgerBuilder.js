0ed87e7857a357b8b9a7ba524bbbc1c8fd41ee60
Leif Guillermo <guillermoleif@gmail.com>
2019-11-10 22:30:30 -0700

    Add axios and the ability to post data to firebase.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -93,7 +94,27 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler() {
-    alert("'Continue' was chosen.");
+    // alert("'Continue' was chosen.");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, // Don't use this in production. Calculate price on the server!
+      customer: {
+        name: 'Leif Guillermo',
+        address: {
+          street: '123 easy street',
+          zipCode: '87108',
+          country: 'USA'
+        }
+      }
+    };
+    axios
+      .post('/orders.json', order)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
